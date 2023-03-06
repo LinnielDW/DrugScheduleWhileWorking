19b3a759bb5bcdd53b5574b06adb8024a1e4d581
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-03-06 14:18:20 +0100

    implemented basic functionality

---
+++ 1.4/Source/DrugScheduleWhileWorking/DrugScheduleWhileWorking.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace DrugScheduleWhileWorking;
+
+public class DrugScheduleWhileWorking : Mod
+{
+    public DrugScheduleWhileWorking(ModContentPack content) : base(content)
+    {
+        var harmony = new Harmony("com.arquebus.rimworld.mod.drugschedulewhilesorking");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ 1.4/Source/DrugScheduleWhileWorking/Patches/JobGiver_TakeDrugsForDrugPolicy_GetPriority_Patch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace DrugScheduleWhileWorking.Patches;
+
+[HarmonyPatch(typeof(JobGiver_TakeDrugsForDrugPolicy), "GetPriority")]
+public static class JobGiver_TakeDrugsForDrugPolicy_GetPriority_Patch
+{
+    static void Postfix(ref float __result)
+    {
+        if (__result > 0) //TODO: add settings here
+        {
+            __result = 9.1f;
+        }
+    }
+}
