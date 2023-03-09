dbb7ba1267c51519b604c2a94c7f9d0029b29b85
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-03-09 21:05:13 +0100

    added settings menu

--- 1.4/Source/DrugScheduleWhileWorking/Patches/JobGiver_TakeDrugsForDrugPolicy_GetPriority_Patch.cs
+++ 1.4/Source/DrugScheduleWhileWorking/Patches/JobGiver_TakeDrugsForDrugPolicy_GetPriority_Patch.cs
@@ -1,16 +1,24 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using Verse;
 
 namespace DrugScheduleWhileWorking.Patches;
 
+//This should officially be a transpiler, but given that I've seen multiple mods mess with drug schedule related functions, I opted to make this a postfix instead.
+//Repeat calculation of ShouldTryToTakeScheduledNow was no concern, neglagable impact on performance.
 [HarmonyPatch(typeof(JobGiver_TakeDrugsForDrugPolicy), "GetPriority")]
 public static class JobGiver_TakeDrugsForDrugPolicy_GetPriority_Patch
 {
-    static void Postfix(ref float __result)
+    static void Postfix(ref float __result, Pawn pawn)
     {
-        if (__result > 0) //TODO: add settings here
+        DrugPolicy currentPolicy = pawn.drugs.CurrentPolicy;
+        for (int i = 0; i < currentPolicy.Count; i++)
         {
-            __result = 9.1f;
+            if (pawn.drugs.ShouldTryToTakeScheduledNow(currentPolicy[i].drug) && DrugScheduleWhileWorkingSettings.drugsEnabledDuringWorkSettingsDictionary[currentPolicy[i].drug.defName].drugEnabledBool)
+            {
+                //9.1f chosen as work and meditation are both at prio 9.
+                __result = 9.1f;
+            }
         }
     }
 }
